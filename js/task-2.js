417bd296a8a3352af8e467fa3bd4b2572aff1290
SCBProd <scbproduktion@gmail.com>
2025-11-19 22:38:41 +0100

    giu

---
+++ js/task-2.js
@@ -0,0 +1,19 @@
+function makeArray(firstArray, secondArray, maxLength) {
+    let pie = firstArray.concat(secondArray);
+    if (pie.length <= maxLength) {
+        return pie;
+    } else { 
+        while (pie.length > maxLength) {
+            pie.pop();
+            
+        }
+        return pie
+        
+    }
+}
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
